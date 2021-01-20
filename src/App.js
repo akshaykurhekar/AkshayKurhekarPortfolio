85ffc9d98f88392ba91b0992b2d0afde12b40406
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-01-20 13:30:14 +0530

    removed warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Particles from './component/practical';
 import profile from './Akshay_Kurhekar.jpeg';
-import { FaGithub,FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
             <img className="image" src={profile} alt="akshayMK" />
             <h1>Akshay Kurhekar &#129505;</h1>
             <FaGithub/>
+            <FaLinkedin/>
             <p> Full Stack Developer &#128540; </p>
             Stay turn..
             Comming soon...    
