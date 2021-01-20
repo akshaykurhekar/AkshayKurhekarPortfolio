884b82e72544942bab3d41d86f7900f78d90a110
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-01-20 14:56:12 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ function App() {
         <div className="card">
             <img className="image" src={profile} alt="akshayMK" />
             <h1>Akshay Kurhekar &#129505;</h1>
-            <FaGithub/>
-            <FaLinkedin/>
+             <a  href="https://github.com/akshaykurhekar" target="_blank"><FaGithub className="icon"/></a> 
+             <a  href="https://www.linkedin.com/in/akshay-kurhekar-64438a168/" target="_blank"><FaLinkedin className="icon"/></a>
+            
+            
             <p> Full Stack Developer &#128540; </p>
             Stay turn..
             Comming soon...    
