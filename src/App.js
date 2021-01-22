e8eee152ce998dd9533fcf009d1b18452ddb33d3
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-01-22 16:32:32 +0530

    showdo added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
           <Particles />
         <div className="card">
             <img className="image" src={profile} alt="akshayMK" />
-            <h1>Akshay Kurhekar &#129505;</h1>
-             <a  href="https://github.com/akshaykurhekar" target="_blank"><FaGithub className="icon"/></a> 
-             <a  href="https://www.linkedin.com/in/akshay-kurhekar-64438a168/" target="_blank"><FaLinkedin className="icon"/></a>
+            <h1>Akshay Manohar Kurhekar &#129505;</h1>
+             <a  href="https://github.com/akshaykurhekar" rel="noreferrer" target="_blank" ><FaGithub className="icon"/></a> 
+             <a  href="https://www.linkedin.com/in/akshay-kurhekar-64438a168/" rel="noreferrer" target="_blank" ><FaLinkedin className="icon"/></a>
             
             
             <p> Full Stack Developer &#128540; </p>
