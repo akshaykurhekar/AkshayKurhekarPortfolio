bf581f3aefca16e53ef57730d62f943ef1193608
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-01-25 17:18:48 +0530

    photo updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Particles from './component/practical';
-import profile from './Akshay_Kurhekar.jpeg';
+import profile1 from './AkshayMK1.jpg';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function App() {
 
-    console.log(profile);
   return (
       <div>
           <div className="card">
-            <img className="image" src={profile} alt="akshayMK" />
+            <img className="image" src={profile1} alt="akshayMK" />
+            
             <div className="heading">Akshay Manohar Kurhekar 👋</div>
              <a id="git" href="https://github.com/akshaykurhekar" rel="noreferrer" target="_blank" ><FaGithub className="icon"/></a> 
              <a id="linked" href="https://www.linkedin.com/in/akshay-kurhekar-64438a168/" rel="noreferrer" target="_blank" ><FaLinkedin className="icon"/></a>
